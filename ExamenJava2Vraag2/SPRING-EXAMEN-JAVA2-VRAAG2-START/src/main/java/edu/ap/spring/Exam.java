5f840f3e7236d41cc7f778f362369e781d3175d0
M-Alem <32704888+M-Alem@users.noreply.github.com>
2018-04-18 15:21:49 +0200

    last
    last commit

--- ExamenJava2Vraag2/SPRING-EXAMEN-JAVA2-VRAAG2-START/src/main/java/edu/ap/spring/Exam.java
+++ ExamenJava2Vraag2/SPRING-EXAMEN-JAVA2-VRAAG2-START/src/main/java/edu/ap/spring/Exam.java
@@ -11,14 +11,14 @@ public class Exam {
 	// priemgetallen terug te geven
 	// 2 punten
 	public int[] getPrimes(int[] numbers) {
-		
+
 	}
 	
 	// Maak gebruik van lambdas en streams alle lowercase characters
 	// te tellen in de gegeven string
 	// 1 punt
 	public int countLowercaseCharacters(String string) {
-        
+
 	}
 	
 	// Maak gebruik van lambdas en streams om de som van alle
